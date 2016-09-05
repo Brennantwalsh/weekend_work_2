df8fc4deb2183a124159ef8a459765c8af2fe206
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-05 14:46:24 -0500

    without reverse methos

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,10 +1,17 @@
 # Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
 
 def palindrome?(string)
-  if string == string.reverse
-    result = true
+  split = string.split(//)
+  position = -1
+  answer = []
+  split.each do
+    answer << split[position]
+    position -= 1
+  end
+  if answer.join == string
+    true
   else
-    result = false
+    false
   end
 end
 
